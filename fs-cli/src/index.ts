7a615cad5c7141ee6169b85228b36744f373053a
Nick Ferraro <the.nick.ferraro@gmail.com>
2023-03-14 16:25:17 -0700

    WIP: Add fs cli to rename file extensions

---
+++ fs-cli/src/index.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import fs from 'fs-extra';
+import yargs from 'yargs';
+
+yargs
+    .scriptName('fs-cli')
+    .command('clean-ext', (y) => {
+
+    }, (args) => {
+
+    })
